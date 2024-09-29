58d3777fb7dfdc43742e182506b17252108c1f07
EDWARD <edward.kang0301@gmail.com>
2024-09-30 08:57:00 +1000

    Linked backend with frontend, added some exception handling, created images and got sky working

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import pandas
 
 API_KEY = os.getenv("openweather")
 
-def get_data(place, forecast_days, kind=None):
+def get_data(place, forecast_days):
     if API_KEY is None:
         raise ValueError("API key must be provided")
 
@@ -15,7 +15,7 @@ def get_data(place, forecast_days, kind=None):
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(f"Error fetching data: {response.status_code} - {response.text}")
+        return "none"
 
     data = response.json()
 
@@ -26,11 +26,7 @@ def get_data(place, forecast_days, kind=None):
     nr_values = 8 * forecast_days # Because data every 3 hours, 24 hours in a day leads to 8 data points
     filtered_data = filtered_data[:nr_values]
 
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data] # List comprehension
-    elif kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=1, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=1))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+ # Add title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -15,8 +16,25 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} {day} in {place}")
 
-data = get_data(place, days, option)
+if place:
+     # Get the temperature / sky data
+    filtered_data = get_data(place, days)
+    if filtered_data == "none":
+         st.write("Error. Please enter a city name.")
 
-d, t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    elif option == "Temperature":
+        # Filter the data
+        temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data] # List comprehension
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create a temperature plot
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+
+    elif option == "Sky":
+        # Filter the data
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png",
+                  "Rain":"images/rain.png", "Snow":"images/snow.png"}
+        # Translation
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width=115)
